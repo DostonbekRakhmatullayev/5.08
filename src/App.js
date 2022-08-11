2ac831c02bb203dbeaa38cdaa17c1a258775935a
Dostonbek Rahmatullayev <rahmatullayivdoston@gmail.com>
2022-08-11 23:44:19 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Home } from "./pages/Home/Home";
+import { About } from "./pages/About/About";
+import { Blog } from "./pages/Bolg/Bolg";
+import { TvShow } from "./pages/TvShow/TvShow";
+import { UpComming } from "./pages/UpComming/UpComming";
+import { Routes, NavLink, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-box">
+        <div className="container">
+          <header className="header">
+            <NavLink className="header__nav-link" to="/">
+              Home Page
+            </NavLink>
+            <NavLink className="header__nav-link" to="/About">
+              Popular Movies
+            </NavLink>
+            <NavLink className="header__nav-link" to="/Bolg">
+              Top-rated Movies
+            </NavLink>
+            <NavLink className="header__nav-link" to="/upComming">
+              Up comming Movies
+            </NavLink>
+            <NavLink className="header__nav-link" to="/tvShow">
+              Tv-show
+            </NavLink>
+          </header>
+        </div>
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/bolg" element={<Blog />} />
+          <Route path="/tvShow" element={<TvShow />} />
+          <Route path="/upComming" element={<UpComming />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/pages/UpComming/UpComming.js
@@ -0,0 +1,44 @@
+import "./upcomming.css";
+import { useEffect, useState } from "react";
+
+export const UpComming = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=5a85e135dc7864482b9a3c9378e912b1`
+    )
+      .then((res) => res.json())
+      .then((dataApi) => setData(dataApi.results));
+  }, []);
+
+  return (
+    <>
+      <ul className="container upocmming-list">
+        {data.length &&
+          data.map((e) => (
+            <li key={e.title} className="upocmming-item">
+              <img
+              className="upocmming-img"
+                src={`https://image.tmdb.org/t/p/w500${e.backdrop_path}`}
+                alt=""
+              />
+              <h3 className="upocmming-title">{e.title}</h3>
+              <time className="upocmming-time">{e.release_date}</time>
+            </li>
+          ))}
+      </ul>
+      <marquee className="marquee-text" behavior="" direction="">
+        1 Rossiya rubli, RUB, Sotib olish(145.00), Sotish(190.00). MB(177.49)
+        *** 2 Yevro, EUR Sotib olish(10500.00), Sotish(11700.00), MB(11052.42)
+        ***3 AQSh dollari, USD Sotib olish(10900.00), Sotish(10960.00),
+        MB(10920.28) *** Mol go'shti 13 ming so'm, *** qo'y go'shti 14-15 ming
+        so'mga yetgan, *** xorijiy o'simlik yog'ining bir litri 5000 so'mdan
+        oshgan, *** Qozog'iston bug'doy unining bir kilogrammi 2000 so'mga
+        sotilayapti. *** Gulistonda bir kilogramm guruch 2000-2500 so'mga
+        yetgan, *** mahalliy paxta yog'ining bir litri 2800-3000 so'mdan
+        sotilayapti.
+      </marquee>
+    </>
+  );
+};
